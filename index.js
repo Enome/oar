7bf3f3bf7b1fdf6b2ab79c3c56e277518d97c6e4
Geert Pasteels <geert.pasteels@gmail.com>
2013-08-01 21:30:51 +0200

    Oar can now be used as a component

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var oar = function (base) {
 
   var arr = base || [];
   var handlers = {};
+  var nextTick = typeof process !== 'undefined' ? process.nextTick : setTimeout;
 
   Object.defineProperty(arr, 'on', { value:  function (event, callback) {
     if (typeof handlers[event] === 'undefined') {
@@ -15,13 +16,13 @@ var oar = function (base) {
     var args = Array.prototype.slice.call(arguments, 1);
     var result = Array.prototype[method].apply(arr, args);
 
-    process.nextTick(function () {
+    nextTick(function () {
       if (typeof handlers[method] !== 'undefined') {
         handlers[method].forEach(function (handler) {
           handler(arr);
         });
       }
-    });
+    }, 0);
 
     return result;
 
